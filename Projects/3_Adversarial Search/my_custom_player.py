d98d635189bce85db3e083ecc52d8c8a93ce6a0e
Eric Law <eric.law@daimler.com>
2020-10-31 15:27:17 -0700

    implemented custom player using MCTS, added alpha beta pruning with iterative deepening agent in sample players

--- Projects/3_Adversarial Search/my_custom_player.py
+++ Projects/3_Adversarial Search/my_custom_player.py
@@ -1,6 +1,9 @@
-
 from sample_players import DataPlayer
+import numpy as np
+import copy, random
 
+# This Monte-Carlo Tree Search was implemented using the following article as a reference:
+# https://medium.com/@quasimik/monte-carlo-tree-search-applied-to-letterpress-34f41c86e238
 
 class CustomPlayer(DataPlayer):
     """ Implement your own agent to play knight's Isolation
@@ -18,6 +21,7 @@ class CustomPlayer(DataPlayer):
     - You can pass state forward to your agent on the next turn by assigning
       any pickleable object to the self.context attribute.
     **********************************************************************
+
     """
     def get_action(self, state):
         """ Employ an adversarial search technique to choose an action
@@ -42,5 +46,82 @@ class CustomPlayer(DataPlayer):
         # EXAMPLE: choose a random move without any search--this function MUST
         #          call self.queue.put(ACTION) at least once before time expires
         #          (the timer is automatically managed for you)
-        import random
-        self.queue.put(random.choice(state.actions()))
+        if state.ply_count < 2:
+            self.queue.put(random.choice(state.actions()))
+        else:
+            root_node = MCTSNode(state)
+            while (True):
+                if root_node.state.terminal_test():
+                    return random.choice(state.actions())
+                child = selection(root_node)
+                reward = simulation(child.state)
+                backpropagation(child, reward)
+                index = root_node.children.index(best_ucb(root_node))
+                best_action = root_node.children_actions[index]
+                self.queue.put(best_action)
+
+# The selection step traverses (by expanding) the current tree and uses the "best_ucb" algorithm
+# to select the best node based on the Upper-Confidence-Bound (UCB)
+def selection(node):
+    while not node.state.terminal_test():
+        if not len(node.children_actions) == len(node.state.actions()):
+            return expansion(node)
+        node = best_ucb(node)
+    return node
+
+
+# The expansion step adds new child node to the tree
+def expansion(node):
+    for action in node.state.actions():
+        if action not in node.children_actions:
+            not_tried_state = node.state.result(action)
+            node.add_child_node(not_tried_state, action)
+            node_child = node.children[-1]
+            return node_child
+
+
+# The simulation step randomly chooses available moves until the game is over
+def simulation(state):
+    player_copy = copy.deepcopy(state.player())
+    while not state.terminal_test():
+        action = random.choice(state.actions())
+        state = state.result(action)
+    if state._has_liberties(player_copy):
+        return -1
+    else:
+        return 1
+
+# The backpropagation step recursively updates all the statistics and rewards the "good"
+# nodes/actions
+def backpropagation(node, reward):
+    node.reward += reward
+    node.visits += 1
+    reward = -reward
+    if node.parent:
+        backpropagation(node.parent, reward)
+
+
+# The MCTS node structure
+class MCTSNode():
+    def __init__(self, state, parent=None):
+        self.state = state
+        self.parent = parent
+        self.visits = 0
+        self.reward = 0
+        self.children = []
+        self.children_actions = []
+
+    def add_child_node(self, state, action):
+        child = MCTSNode(state, self)
+        self.children.append(child)
+        self.children_actions.append(action)
+
+
+# This implements the UCB to return the weight
+def best_ucb(node):
+    values = [
+        (child.reward / child.visits) + np.sqrt(2. * np.log(node.visits) / child.visits)
+        for child in node.children
+    ]
+
+    return node.children[np.argmax(values)]
--- Projects/3_Adversarial Search/sample_players.py
+++ Projects/3_Adversarial Search/sample_players.py
@@ -133,3 +133,87 @@ class MinimaxPlayer(BasePlayer):
         own_liberties = state.liberties(own_loc)
         opp_liberties = state.liberties(opp_loc)
         return len(own_liberties) - len(opp_liberties)
+
+
+class AlphaBetaPlayer(BasePlayer):
+    """ Implement your own agent to play knight's Isolation
+
+    The get_action() method is the only required method for this project.
+    You can modify the interface for get_action by adding named parameters
+    with default values, but the function MUST remain compatible with the
+    default interface.
+
+    **********************************************************************
+    NOTES:
+    - The test cases will NOT be run on a machine with GPU access, nor be
+      suitable for using any other machine learning techniques.
+
+    - You can pass state forward to your agent on the next turn by assigning
+      any pickleable object to the self.context attribute.
+    **********************************************************************
+    """
+    def get_action(self, state):
+        """ Choose an action available in the current state
+
+        See RandomPlayer and GreedyPlayer for examples.
+
+        This method must call self.queue.put(ACTION) at least once, and may
+        call it as many times as you want; the caller is responsible for
+        cutting off the function after the search time limit has expired.
+
+        **********************************************************************
+        NOTE: since the caller is responsible for cutting off search, calling
+              get_action() from your own code will create an infinite loop!
+              See (and use!) the Isolation.play() function to run games.
+        **********************************************************************
+        """
+        # randomly select a move as player 1 or 2 on an empty board, otherwise
+        # return the optimal minimax move at a fixed search depth of 3 plies
+        if state.ply_count < 2:
+            self.queue.put(random.choice(state.actions()))
+        else:
+          depth = 1
+          while (True):
+              best_move = self.alpha_beta_search(state, depth)
+              self.queue.put(best_move)
+              depth += 1
+
+    def alpha_beta_search(self, state, depth):
+        """ Return the move along a branch of the game tree that
+        has the best possible value.  A move is a pair of coordinates
+        in (column, row) order corresponding to a legal move for
+        the searching player.
+
+        You can ignore the special case of calling this function
+        from a terminal state.
+        """
+        def min_value(state, depth, alpha, beta):
+            if state.terminal_test(): return state.utility(self.player_id)
+            if depth <= 0: return self.score(state)
+            value = float("inf")
+            for action in state.actions():
+                value = min(value, max_value(state.result(action), depth - 1, alpha, beta))
+                if value <= alpha:
+                    return value
+                beta = min(beta, value)
+            return value
+
+        def max_value(state, depth, alpha, beta):
+            if state.terminal_test(): return state.utility(self.player_id)
+            if depth <= 0: return self.score(state)
+            value = float("-inf")
+            for action in state.actions():
+                value = max(value, min_value(state.result(action), depth - 1, alpha, beta))
+                if value >= beta:
+                    return value
+                alpha = max(alpha, value)
+            return value
+
+        return max(state.actions(), key=lambda x: min_value(state.result(x), depth - 1, float("-inf"), float("inf")))
+
+    def score(self, state):
+        own_loc = state.locs[self.player_id]
+        opp_loc = state.locs[1 - self.player_id]
+        own_liberties = state.liberties(own_loc)
+        opp_liberties = state.liberties(opp_loc)
+        return len(own_liberties) - len(opp_liberties)
